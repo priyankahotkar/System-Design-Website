957baebf559fd45e86b31141cdd492ee282f532f
priyankahotkar <160253899+priyankahotkar@users.noreply.github.com>
2025-09-27 18:24:09 +0530

    Headers change

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ const corsOptions = {
 };
   
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // handle preflight  
+app.options(/.*/, cors(corsOptions));
 app.use(express.json());
 
 // Routes
